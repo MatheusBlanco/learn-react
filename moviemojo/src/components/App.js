c98ef03e8078cb5005d66b8b71916fc24f4a61fa
MatheusBlanco <msallesblanco@gmail.com>
2018-12-17 18:11:38 -0200

    Adds movie gallery

--- moviemojo/src/components/App.js
+++ moviemojo/src/components/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Header from './Header';
+import Movie from './Movie';
+import {initialMovies} from '../movies';
+import {additionalMovies} from '../movies';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header text="Something something" />
-        <p className="App-intro">
-          Welcome to the 'Movie Mojo' React app!
-        </p>
+        <Header text="Discover your Movie Mojo" />
+        <p className="App-intro">Sharing a few of our favorite movies</p>
+        <div className="movies">
+          {
+            Object
+              .keys(this.state.movies)
+              .map(key => <Movie key={key} meta={this.state.movies[key]} />)
+          }
+        </div>
       </div>
     );
+  }
+
+  constructor(){
+    super();
+
+    this.state = {
+      movies: initialMovies
+    };
   }
 }
 
