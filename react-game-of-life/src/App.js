6054ce52ec08ea1bafe9881e061fd93f30fcba9b
MatheusBlanco <msallesblanco@gmail.com>
2019-05-22 14:45:10 -0300

    game of life

---
+++ react-game-of-life/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './Game.js';
+
+function App() {
+  return (
+    <div className="App">
+          <br />
+          <Game/>
+    </div>
+  );
+}
+
+export default App;
