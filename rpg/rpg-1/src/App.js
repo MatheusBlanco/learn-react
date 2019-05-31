9927009ae1b57559c3fcade44346e7f8562237b3
MatheusBlanco <msallesblanco@gmail.com>
2019-05-30 22:13:41 -0300

    commit 1

---
+++ rpg/rpg-1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Player from './features/player'
+import Map from './features/map'
+
+function App() {
+  return (
+    <div>
+      <Map/>
+      <Player/>
+    </div>
+  )
+}
+
+
+export default App;
